33485fd7d38d1a257eb95f6f7421962ac011494b
George Perry <gperry@nd.edu>
2023-04-20 17:27:16 -0400

    Milestone06 final updates

--- Project06/fs.c
+++ Project06/fs.c
@@ -1,37 +1,41 @@
+/*
+Implementation of SimpleFS.
+Make your changes here.
+*/
 
 #include "fs.h"
 #include "disk.h"
 
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <unistd.h>
+#include <stdint.h>
 
-#define FS_MAGIC           0xf0f03410
+extern struct disk *thedisk;
+
+#define FS_MAGIC           0x30341003
 #define INODES_PER_BLOCK   128
-#define POINTERS_PER_INODE 5
+#define POINTERS_PER_INODE 3
 #define POINTERS_PER_BLOCK 1024
 
 struct fs_superblock {
-	int magic;
-	int nblocks;
-	int ninodeblocks;
-	int ninodes;
+	int32_t magic;
+	int32_t nblocks;
+	int32_t ninodeblocks;
+	int32_t ninodes;
 };
 
 struct fs_inode {
-	int isvalid;
-	int size;
-	int direct[POINTERS_PER_INODE];
-	int indirect;
+	int32_t isvalid;
+	int32_t size;
+	int64_t ctime;
+	int32_t direct[POINTERS_PER_INODE];
+	int32_t indirect;
 };
 
 union fs_block {
 	struct fs_superblock super;
 	struct fs_inode inode[INODES_PER_BLOCK];
 	int pointers[POINTERS_PER_BLOCK];
-	char data[DISK_BLOCK_SIZE];
+	unsigned char data[BLOCK_SIZE];
 };
 
 int fs_format()
@@ -42,26 +46,53 @@ int fs_format()
 void fs_debug()
 {
 	union fs_block block;
+	disk_read(thedisk, 0, block.data);
 
-	disk_read(0,block.data);
+	// Check that nblocks, ninodeblocks, and ninodes are consistent
+    int expected_ninodeblocks = (block.super.ninodes + INODES_PER_BLOCK - 1) / INODES_PER_BLOCK;
+	int expected_ninodes = expected_ninodeblocks * INODES_PER_BLOCK;
+
+    if (block.super.nblocks != thedisk->nblocks ||
+        block.super.ninodeblocks != expected_ninodeblocks ||
+        block.super.ninodes != expected_ninodes) 
+	{
+        printf("    Error - system inconsistent\n");
+        printf("    exiting...\n");
+        return;
+    }
 
 	printf("superblock:\n");
+
+    // Check if magic number is valid
+    if(block.super.magic == FS_MAGIC) 
+        printf("    magic number is valid\n");
+    else {
+        printf("    magic number is invalid\n");
+        printf("    exiting...\n");
+        return;
+    }
+
 	printf("    %d blocks\n",block.super.nblocks);
 	printf("    %d inode blocks\n",block.super.ninodeblocks);
 	printf("    %d inodes\n",block.super.ninodes);
 
+    // Read through all the inode blocks
     for (int i = 1; i <= block.super.ninodeblocks; i++) {
-        disk_read(i, block.data);
+        disk_read(thedisk, i, block.data);
 
+        // go through inodes in the current block
         for (int j = 0; j < INODES_PER_BLOCK; j++) {
 
+            // calculate current inode #
             int inum = (i - 1) * INODES_PER_BLOCK + j;
 
+            // check if it's valid, print out info
             if (block.inode[j].isvalid) {
                 printf("inode %d:\n", inum);
                 printf(" size: %d bytes\n", block.inode[j].size);
                 printf(" direct blocks:");
 
+                // go through all the direct blocks, print them out
                 for (int k = 0; k < POINTERS_PER_INODE; k++) {
                     if (block.inode[j].direct[k]) {
                         printf(" %d", block.inode[j].direct[k]);
@@ -70,13 +101,16 @@ void fs_debug()
 
                 printf("\n");
 
+                // check if there are indirect blocks - print info
                 if (block.inode[j].indirect) {
                     printf(" indirect block: %d\n", block.inode[j].indirect);
                     printf(" indirect data blocks:");
 
+                    // create new fs_block union for the indirect blocks
                     union fs_block indirect_block;
-                    disk_read(block.inode[j].indirect, indirect_block.data);
+                    disk_read(thedisk, block.inode[j].indirect, indirect_block.data);
 
+                    // print out the indirect blocks
 					for (int k = 0; k < POINTERS_PER_BLOCK; k++) {
 						if (indirect_block.pointers[k]) {
 							printf(" %d", indirect_block.pointers[k]);
@@ -89,7 +123,6 @@ void fs_debug()
         }
     }
 }
-
 
 
 int fs_mount()
@@ -109,7 +142,7 @@ int fs_delete( int inumber )
 
 int fs_getsize( int inumber )
 {
-	return -1;
+	return 0;
 }
 
 int fs_read( int inumber, char *data, int length, int offset )
